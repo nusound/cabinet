685eb62d5bb3e177bf5b0aa1b52babc13eecfc25
nusound <uroshni77@gmail.com>
2017-09-09 14:13:34 +0200

    Association between users and files

--- db/migrate/20170909115333_devise_create_users.rb
+++ db/migrate/20170909115333_devise_create_users.rb
@@ -1,6 +1,6 @@
-class DeviseCreateUseers < ActiveRecord::Migration[5.1]
+class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :useers do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -34,9 +34,9 @@ class DeviseCreateUseers < ActiveRecord::Migration[5.1]
       t.timestamps null: false
     end
 
-    add_index :useers, :email,                unique: true
-    add_index :useers, :reset_password_token, unique: true
-    # add_index :useers, :confirmation_token,   unique: true
-    # add_index :useers, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909114034) do
+ActiveRecord::Schema.define(version: 20170909115857) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
-  create_table "useers", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -32,8 +33,8 @@ ActiveRecord::Schema.define(version: 20170909114034) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_useers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_useers_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
